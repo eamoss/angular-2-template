eb927d03600322761626a37d2eea1427d5fdc7ee
eamoss <eamonnm@gmx.com>
2017-01-29 23:51:39 +0000

    Adding updates after completing video

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {PlaylistComponent} from './playlist.component';
+import {Video} from './video';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos: Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1, "Test video 1", "f8qBeaGe2S4", "somedesc"),
+            new Video(2, "Test video 2", "bKWDKmHvF78", "somedesc")
+        ]
+    }
+}
